02ad7aea03477d0a2a5404ebe47adfc8a3673e0e
Alberto Monroy <firewind3882@gmail.com>
2022-11-01 16:40:20 -0600

    added the middleware

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+//required modules
 const express = require("express");
 let uniquid = require("uniquid");
 const fs = require("fs");
+let db = require("./db/db.json");
+
+//middleware
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
