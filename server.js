78846528bdc3cd21eb0686b0d5c06833b01ddb2e
Alberto Monroy <firewind3882@gmail.com>
2022-11-01 17:47:25 -0600

    added the get routes

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Routes
+//Get routes
+app.get("/", (req, res) => {
+  res.send("please head over to localhost:3001/api/notes");
+});
+
+app.get("/api/notes", (req, res) => res.json(db));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
+});
