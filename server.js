aa0482e8f87e1dced4a73c65b9e27f92aab5e5ef
Alberto Monroy <firewind3882@gmail.com>
2022-11-01 18:15:31 -0600

    added the post request

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Routes
+
 //Get routes
-app.get("/api", (req, res) => {
-  res.send("please head over to localhost:3001/api/notes");
-});
 
 app.get("/api/notes", (req, res) => res.json(db));
 
@@ -24,6 +22,30 @@ app.get("/notes", (req, res) => {
   res.sendFile(__dirname + "/public/notes.html");
 });
 
+//Post routes
+
+app.post("/api/notes", (req, res) => {
+  if (req.body) {
+    const newNote = {
+      id: uniquid(),
+      title,
+      text,
+    };
+    db.push(newNote);
+    writeToFile("./db/db.json", db);
+    res.json("Note was added");
+  } else {
+    res.errored("Note could not be added!");
+  }
+});
+
+//Write to file
+const writeToFile = (dest, cont) =>
+  fs.writeFile(dest, JSON.stringify(cont, null, 4), (err) =>
+    err ? console.log(err) : console.info(`\nData written to ${destination}`)
+  );
+
+//app.listen
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
